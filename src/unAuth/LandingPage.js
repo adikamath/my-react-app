a5f16fd1c94586d861f241e8bdf4849359450977
Aditya Kamath <adikamath92@gmail.com>
2025-10-13 19:02:19 -0700

    Added Landing Page edits using Cursor

---
+++ src/unAuth/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="container">
+        <h1 className="hello-world">Hello World!!</h1>
+        <p className="subtitle">Welcome to my first React app.</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
